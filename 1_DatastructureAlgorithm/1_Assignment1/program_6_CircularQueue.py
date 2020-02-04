5a4d74adc26569fcf37ab1e551ecdc00b55b52e6
ThyMakra <45727563+ThyMakra@users.noreply.github.com>
2020-02-04 22:48:57 +0700

    Finished program_4

--- 1_DatastructureAlgorithm/1_Assignment1/program_6_CircularQueue.py
+++ 1_DatastructureAlgorithm/1_Assignment1/program_6_CircularQueue.py
@@ -13,58 +13,63 @@ class CircularQueue:
 
     def enqueue(self, item):
         assert not len(self) >= self.size, "Maximum size exceeded! Cannot enqueue more elements in to the queue."
-        self._items.append(item)
         node = _CircularQueueNode(item)
-        
         if self.isEmpty():
+            node.behind = node
+            node.front = node
             self.first = node
-            # self.last
-            
         else:
-            node.back = self.first
+            node.behind = self.first
             node.front = self.last
-            
-            self.last.back = node
-
-            # self.first.back = node
-            # self.first.front = self.last
-            # self.last.back = self.first
-        # if self.last is not None:
-        #     self.last.front
-        
+            if len(self) == 1:
+                self.first.behind = node
+            self.last.behind = node
         self.last = node
         self.first.front = self.last
-            # self.last.front = self.
-            
-            
-        # self.last = node
-        
-        
+        self._items.append(item)
 
-    # def dequeue(self):
-    #     assert not self.isEmpty(), "can not dequeue from an empty queue"
-    #     node = self._qhead
-    #     if self._qhead is self._qtail:
-    #         self._qtail = None
-    #     self._qhead = self._qhead.next
-    #     self._count -= 1
-    #     return node.item
-
-    # def next(self):
-
+    def dequeue(self):
+        assert not self.isEmpty(), "Cannot dequeue from an empty queue"
+        # removed = self.first
+        if len(self) == 1:
+            self.first = None
+            self.last = None
+        self.first = self.first.behind
+        self.first.front = self.last
+        self.last.behind = self.first
+        return self._items.pop(0)
 
 class _CircularQueueNode:
     def __init__(self, item):
         self.item = item
         self.front = None
-        self.back = None
+        self.behind = None
 
-cQ = CircularQueue(1)
-cQ.enqueue(5)
+# create a queue of size 3
+cQ = CircularQueue(3)
+# added 3 elements
+cQ.enqueue("A")
+cQ.enqueue("B")
+cQ.enqueue("C")
+# see how the queue goes from the first element in a circle
+print("See how the queue goes from the first element in a circle")
+print(cQ.first.item)
+print(cQ.first.behind.item)
+print(cQ.first.behind.behind.item)
+print(cQ.first.behind.behind.behind.item)
+print(cQ.first.behind.behind.behind.behind.item)
+# see how the queue goes from the last element in a reverse-circle
+print("See how the queue goes from the last element in a reverse-circle")
+print(cQ.last.item)
+print(cQ.last.front.item)
+print(cQ.last.front.front.item)
+print(cQ.last.front.front.front.item)
+print(cQ.last.front.front.front.front.item)
+# remove an element from the queue
+print("Remove an element from the queue")
+dequeued = cQ.dequeue()
+print(dequeued)
 
-print(cQ)
-print(cQ.head)
-print(cQ.tail)
         
         
 
